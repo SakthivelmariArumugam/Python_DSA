fcea781a0034ac0fccfeadd5114d26432dd0df39
SakthivelmariArumugam <sakthivelmaribe@gmail.com>
2025-05-10 14:52:50 +0530

    multiplicatoin table logic is created

---
+++ logic_easy_problem/Multiplication_table.py
@@ -0,0 +1,9 @@
+
+def Multiplication_Table(n):
+  for i in range(1,11):
+    num=i*n
+    print(f"{i}*{n}={num}")
+
+if __name__=="__main__":
+  n=int(input("Enter the number:"))
+  Multiplication_Table(n)
